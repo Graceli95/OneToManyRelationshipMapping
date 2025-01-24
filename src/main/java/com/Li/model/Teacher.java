e5b5dd47eccfc39d151e2f957ed629859bfacb27
Graceli95 <graceli9095@gmail.com>
2025-01-24 16:08:34 -0700

    One to one relationship mapping

--- src/main/java/com/Li/model/Teacher.java
+++ src/main/java/com/Li/model/Teacher.java
@@ -6,6 +6,24 @@ import jakarta.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 
+/**
+ * ● The @OneToOne annotation is used to map the source entity with the target entity.
+ * ● The one-to-one association can be either unidirectional or bidirectional.
+ * ○ In unidirectional association, the source entity has a relationship field that
+ * refers to the target entity, and the source entity’s table contains the foreign
+ * key.
+ * ○ In a bidirectional association, each entity (e.g., source and target) has a
+ * relationship field that refers to each other, and the target entity’s table
+ * contains the foreign key. The source entity must use the mappedBy attribute
+ * to define the bidirectional one-to-one mapping.
+ * ● In this lab, we will implement only unidirectional entity mapping using
+ * Hibernate.
+ * Let us consider an example of a relationship between a Person and an Address
+ * entity. A person can have one address and that address belongs to one person
+ * only. That is a typical example of a one-to-one relationship or association. We
+ * will model this in a database, and we will need to store the primary key of the
+ * Address record as a foreign key in the Person table.
+ */
 @Entity
 @Table
 public class Teacher implements Serializable {
