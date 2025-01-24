a1fddfa1a536206dd0dc1c9dc15cd1986a43d296
Graceli95 <graceli9095@gmail.com>
2025-01-24 15:04:07 -0700

    One to many relationship mapping

--- src/main/java/com/Li/Main.java
+++ src/main/java/com/Li/Main.java
@@ -1,7 +1,52 @@
 package com.Li;
 
+import com.Li.model.Department;
+import com.Li.model.Teacher;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        oneTOMany();
+        
+    }
+
+    private static void oneTOMany() {
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        //creating teacher
+        Teacher t1 = new Teacher("1000","MHaseeb");
+        Teacher t2 = new Teacher("2220","Shahparan");
+        Teacher t3 = new Teacher("3000","James");
+        Teacher t4 = new Teacher("40000","Joseph");
+        Teacher t5 = new Teacher("200","Ali");
+
+        //Add teacher Entity object to the List
+        ArrayList<Teacher> teacherList = new ArrayList<>();
+        teacherList.add(t1);
+        teacherList.add(t2);
+        teacherList.add(t3);
+        teacherList.add(t4);
+        teacherList.add(t5);
+        session.persist(t1);
+        session.persist(t2);
+        session.persist(t3);
+        session.persist(t4);
+        session.persist(t5);
+
+       //creating Department
+        Department department = new Department();
+        department.setDeptName("Development");
+        department.setTeacherList(teacherList);
+
+        //Storing Department
+        session.persist(department);
+        transaction.commit();
     }
 }
--- src/main/java/com/Li/model/Department.java
+++ src/main/java/com/Li/model/Department.java
@@ -1,4 +1,73 @@
 package com.Li.model;
 
-public class Department {
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Cascade=CascadeType.ALL: Any change that happens to the teacher must cascade
+ * to the department as well. If you save a Teacher, all associated accounts will also be
+ * saved into the database. If you delete a Teacher, all accounts associated with that
+ * Teacher will also be deleted.
+ *
+ * targetEntity=Teacher.class: It is used to determine the entity class that is
+ * the target of the association.
+ * targetEntity: Specify the entity class that is the target of the association.
+ * This is optional only if the collection property is defined using Java generics;
+ * otherwise, it must be specified.
+ */
+
+
+@Entity
+@Table
+public class Department implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int deptId;
+    private String deptName;
+
+    @OneToMany(targetEntity = Teacher.class, cascade = CascadeType.ALL) //We wanted to save the mapped entity whenever the relationship owner entity is saved.To enable this, we had to use the “CascadeType” attribute.
+    private List<Teacher> teacherList;
+
+    public Department(int deptId, String deptName) {
+        super();
+        this.deptId = deptId;
+        this.deptName = deptName;
+    }
+
+    public Department() {}
+
+    public Department(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public int getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(int deptId) {
+        this.deptId = deptId;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public List<Teacher> getTeacherList() {
+        return teacherList;
+    }
+
+    public void setTeacherList(List<Teacher> teacherList) {
+        this.teacherList = teacherList;
+    }
 }
